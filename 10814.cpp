ae4f026dd5a2b507348a494531c229f36c9a8550
JungHyeokMoon <wjdgur9534@daum.net>
2019-02-20 17:09:04 +0900

    10814 나이순정렬

--- 10814.cpp
+++ 10814.cpp
@@ -29,3 +29,39 @@ int main() {
 
 	return 0;
 }
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Person {
+	int age;
+	string name;
+	int join;
+};
+
+bool cmp(const Person &u, const Person &v) {
+	return u.age < v.age;
+}
+int main() {
+	int n;
+	scanf("%d", &n);
+	Person *arr = new Person[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i].age >> arr[i].name;
+		arr[i].join = i;
+	}
+
+	sort(arr, arr + n, [](Person &u, Person&v) {
+		return (u.age < v.age) || (u.age == v.age&&u.join < v.join); //람다식이용.. 잘모르겠음
+	});
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i].age <<" "<<arr[i].name << '\n';//endl이랑 \n이랑 시간차이가있다 이거 유의하자
+	}
+
+	return 0;
+}
