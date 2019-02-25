0175a54dbd35b93cf4038df805be3c88714e721b
JungHyeokMoon <wjdgur9534@daum.net>
2019-02-25 22:05:39 +0900

    2752 세수정렬

---
+++ 2752.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+ 
+int main(){
+    int arr[3];
+    for(int i=0; i<3; i++)
+        scanf("%d", &arr[i]);
+
+    sort(arr,arr+3);
+
+    for(int i=0; i<3; i++)
+        printf("%d ",arr[i]);
+
+    printf("\n");
+
+    return 0;
+}
