68024217726d0e26e5e30a31d49a0474f79f6197
JungHyeokMoon <wjdgur9534@daum.net>
2019-02-28 00:38:53 +0900

    15963 CASIO

---
+++ 15963.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string n,m;
+    cin>>n>>m;
+
+    if(n==m)
+        printf("1\n");
+    else
+        printf("0\n");
+
+return 0;
+    
+}
