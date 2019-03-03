07edeb4a4e09e82dfdb0b94a9c314fbcd9773edc
JungHyeokMoon <wjdgur9534@daum.net>
2019-03-03 18:19:45 +0900

    2744 대소문자바꾸기

---
+++ 2744.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+
+    int len=s.size();
+    for(int i=0; i<len; i++){
+        if('A'<=s[i]&&s[i]<='Z')
+            cout<<(char)(s[i]-'A'+'a');
+        else if('a'<=s[i]&&s[i]<='z')
+            cout<<(char)(s[i]-'a'+'A');
+    }
+    printf("\n");
+}
