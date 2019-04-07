f1c575c7cb1e73dc02519e2c8712ff2955479a00
JungHyeokMoon <wjdgur9534@daum.net>
2019-04-07 23:17:25 +0900

    14645 와이버스부릉부릉

---
+++ 14645.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,k;
+    scanf("%d %d", &n, &k);
+    for(int i=1; i<=n; i++){
+     int in,out;
+     scanf("%d %d", &in, &out);
+    
+     if(i==3)printf("ºñ¿ÍÀÌ\n");   
+    
+    }
+    return 0;
+}
