a8273bc48cd28ef752ee3fa1b8e224fc3e286484
JungHyeokMoon <wjdgur9534@daum.net>
2019-07-09 15:50:57 +0900

    10773 제로

---
+++ 10773.cpp
@@ -0,0 +1,35 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int k;
+stack<int> stk;
+int sum = 0;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> k;
+	for (int i = 0; i < k; i++) {
+		int num;
+		cin >> num;
+		if (num != 0) {
+			stk.push(num);
+		}
+		else {
+			stk.pop();
+		}
+	}
+	while (!stk.empty()) {
+		sum += stk.top();
+		stk.pop();
+	}
+	cout << sum << endl;
+
+	return 0;
+}
