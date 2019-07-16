a628e7b890fdadb393f1339428438675d911516f
JungHyeokMoon <wjdgur9534@daum.net>
2019-07-16 16:50:00 +0900

    algospot_mercy

---
+++ algospot_mercy.cpp
@@ -0,0 +1,29 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <string.h>
+#include <math.h>
+using namespace std;
+const int MAX = 2200;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << "Hello Algospot!" << endl;
+	}
+	return 0;
+}
+
+
+
+
