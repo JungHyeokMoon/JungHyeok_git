f315c89ff0d794c0362739dea25cf2b756a83141
JungHyeokMoon <wjdgur9534@daum.net>
2019-07-24 12:18:01 +0900

    15469 n과m1

---
+++ 15649.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+int n, m;
+vector<int> arr;
+const int MAX = 9;
+bool visited[MAX];
+
+void p(int depth) {
+	if (depth == m) {
+		for (int i = 0; i < arr.size(); i++) {
+			cout << arr[i] << " ";
+		}
+		cout <<'\n';
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (visited[i] == true)continue;
+		arr.push_back(i);
+		visited[i] = true;
+
+		p(depth + 1);
+
+		arr.pop_back();
+		visited[i] = false;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	std::cout.tie(NULL);
+	cin >> n >> m;
+	p(0);
+}
