b3065ff7a3a074e331a74d6731975666855e5951
JungHyeokMoon <wjdgur9534@daum.net>
2019-08-19 18:25:02 +0900

    11055 가장큰 증가부분수열

---
+++ 11055.cpp
@@ -0,0 +1,43 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string.h>
+#include <map>
+#include <set>
+using namespace std;
+
+int n;
+const int MAX = 1000;
+int dp[MAX + 1];
+int arr[MAX + 1];
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	int ret = 0;
+	for (int i = 0; i < n; i++) {
+		dp[i] = arr[i];
+		for (int j = 0; j < i; j++) {
+			if (arr[j] < arr[i]) {
+				dp[i] = max(dp[i], dp[j] + arr[i]);
+			}
+		}
+		ret = max(ret, dp[i]);
+	}
+
+	cout << ret << "\n";
+	
+	
+	return 0;
+}
