c53d8237e6d3a4711019c30f878965d2e68f8528
JungHyeokMoon <wjdgur9534@daum.net>
2019-08-20 15:53:49 +0900

    1463 1로만들기

--- 1463.cpp
+++ 1463.cpp
@@ -1,46 +1,41 @@
 #pragma warning(disable:4996)
 #include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
 #include <algorithm>
+#include <string.h>
+#include <map>
+#include <set>
 using namespace std;
 
-int dp[1000000 + 1];//다이나믹테이블 1을만들기위한 최소한의연산수가 들어가있다.  연산을 사용하는 횟수의 최솟값을 출력하시오.
-/*int main() {
-	int n;
-	scanf("%d", &n);
-	dp[1] = 0;
-	for (int i = 2; i <= n; i++) {
-		dp[i] = dp[i - 1] + 1;
-		if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) //if(i%2==0) dp[i]=min(dp[i],dp[i/2]+1) 여기서 min안에있는 dp[i]는 dp[i] = dp[i - 1] + 1;요거
-			dp[i] = dp[i / 2] + 1;
-		if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) //if(i%3==0) dp[i]=min(dp[i],dp[i/3]+1) 위에와마찬가지로 min안에있는 dp[i] 는 dp[i] = dp[i - 1] + 1;
-			dp[i] = dp[i / 3] + 1;
-	}
-	printf("%d\n", dp[n]);
+const int MAX = 1000000;
+int dp[MAX + 1];
+int n;
 
-	return 0;
-}*///bottom up
 
-/*int solution(int n) {
-	if (n <= 1)
-		return 0;
-	if (dp[n] > 0)
-		return dp[n];
-	dp[n] = solution(n - 1) + 1;//n을 n-1로만들어서 1로만드는경우의수
-	if (n % 2 == 0) {
-		int temp = solution(n / 2) + 1;
-		if (dp[n] > temp) dp[n] = temp;
-	}
-	if (n % 3 == 0) {
-		int temp = solution(n / 3) + 1;
-		if (dp[n] > temp)dp[n] = temp;
-	}
-	return dp[n];
+int solve(int num) {
+	if (num == 1) return 0;
+
+	int &ret = dp[num];
+
+	if (ret != -1)return ret;
+
+	int result = solve(num - 1) + 1;
+	if (num % 3 == 0)result = min(result, solve(num / 3) + 1);
+	if (num % 2 == 0) result = min(result, solve(num / 2) + 1);
+
+	return ret = result;
 }
 int main() {
-	int n;
-	scanf("%d", &n);
-	printf("%d\n", solution(n));
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	memset(dp, -1, sizeof(dp));
+	
+	cin >> n;
+
+	cout << solve(n) << "\n";
 	return 0;
-
-}*/ //topdown
+}
