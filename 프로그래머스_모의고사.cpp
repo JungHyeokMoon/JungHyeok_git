44c5cb23612648a072105769fd52c96800c323ed
MoonJungHyeok <wjdgur9534@daum.net>
2020-02-25 15:49:23 +0900

    프로그래머스 모의고사

--- 프로그래머스_모의고사.cpp
+++ 프로그래머스_모의고사.cpp
@@ -1,18 +1,11 @@
 #include <bits/stdc++.h>
 #define endl "\n"
 using namespace std;
-bool compare(const pair<int,int> &a, const pair<int,int> &b){
-    if(a.first>=b.first && a.second<=b.second){
-        return true;
-    }
-    return false;
-}
 vector<int> solution(vector<int> answers) {
     int A[5]={1,2,3,4,5};
-    int B[6]={2,1,2,3,2,5};
+    int B[8]={2,1,2,3,2,4,2,5};
     int C[10]={3,3,1,1,2,2,4,4,5,5};
     int length=answers.size();
-    vector<pair<int,int>> v;
     int A_count=0;
     int B_count=0;
     int C_count=0;
@@ -20,26 +13,24 @@ vector<int> solution(vector<int> answers) {
         if(answers[i]==A[i%5]){
             A_count++;
         }
-        if(answers[i]==B[i%6]){
+        if(answers[i]==B[i%8]){
             B_count++;
         }
         if(answers[i]==C[i%10]){
             C_count++;
         }
     }
-    v.push_back({A_count,1});
-    v.push_back({B_count,2});
-    v.push_back({C_count,3});
-    sort(v.begin(),v.end(),compare);
-
-    
     vector<int> answer;
-    answer.push_back(v[0].second);
-    if(v[0].first==v[1].first){
-        answer.push_back(v[1].second);
-        if(v[1].first==v[2].first){
-            answer.push_back(v[2].second);
-        }
+    int max_val=max(C_count,max(A_count,B_count));
+    if(max_val==A_count){
+        answer.push_back(1);
+    }
+    if(max_val==B_count){
+        answer.push_back(2);
+    }
+    if(max_val==C_count){
+        answer.push_back(3);
     }
     return answer;
 }
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
