2287dc19a4e8502e335b0e2c574de889c24e9f1e
MoonJungHyeok <wjdgur9534@daum.net>
2020-02-28 19:57:54 +0900

    프로그래머스_비밀지도

--- 프로그래머스_비밀지도.cpp
+++ 프로그래머스_비밀지도.cpp
@@ -33,4 +33,27 @@ vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
 
     return answer;
 }
+
+
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    for(int i=0; i <n; i++){
+        arr1[i] = arr1[i]|arr2[i];
+        // cout<<"arr1[i]: "<<arr1[i]<<endl;
+        string ans = "";
+        for(int j = 0; j<n; j++){
+            if(arr1[i] % 2 == 0) ans = " " + ans;
+            else ans = "#" + ans;
+            arr1[i] = arr1[i] >> 1;
+        }
+        answer.push_back(ans);
+    }
+    return answer;
+}
 //출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
