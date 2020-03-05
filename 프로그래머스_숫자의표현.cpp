512bacb9c5f5a6058485ffddf2dd3e6b5a34c013
MoonJungHyeok <wjdgur9534@daum.net>
2020-03-06 02:05:07 +0900

    프로그래머스_숫자의표현

---
+++ 프로그래머스_숫자의표현.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int i=1; i<=n; i++){
+        int temp=i;
+        // cout<<"i:"<<i<<endl;
+        int num=0;
+        while(num<n){
+            num+=temp++;
+            // cout<<temp<<endl;
+        }
+        if(num==n){
+            answer++;
+        }
+    }
+    return answer;
+}
