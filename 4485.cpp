82e873e30a7170165237f8c2e44ba6bd741d6a7d
문상혁 <msk2292@naver.com>
2020-03-09 22:17:26 +0900

    4485 녹색옷 입은애가 젤다지?

--- 4485.cpp
+++ 4485.cpp
@@ -2,47 +2,26 @@
 using namespace std;
 int n;
 int arr[126][126];
-int dist[126];
+int dist[126][126];
 #define endl "\n"
 #define MAX 987654321
-void Init(){
-    for(int i=0; i<n; i++){
-        fill(arr[i],arr[i]+n,0);
+typedef pair<int,pair<int,int>> piii;
+const int dy[4]={0,-1,0,1};
+const int dx[4]={-1,0,1,0};
+bool isinside(int y, int x){
+    if(y>=0 && y<n &&x>=0 && x<n){
+        return true;
     }
-    // for(int i=0; i<n; i++){
-    //     fill(dist[i],dist[i]+n,987654321);
-    // }
-    fill(dist,dist+n,MAX);
+    return false;
 }
-void Problem(int i){
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-    pq.push({arr[0][0],0}); //도둑루피, vertex
-    dist[0]=arr[0][0];
-    while(!pq.empty()){
-        int hc=pq.top().first;
-        int hv=pq.top().second;
-        cout<<i+1<<" : "<<hc<<", "<<hv<<endl;
-        pq.pop();
-        if(dist[hv]<hc)continue;  //이렇게되면 뒤에 더 코드가 진행되도 갱신될 여지가없음
 
-        for(int i=0; i<n ; i++){
-            // if(hc==i)continue;
-            int nv=i;
-            int nc=hc+arr[hv][i];
-            if(nc<dist[nv]){
-                dist[nv]=nc;
-                pq.push({nc,nv});
-            }
-        }
+void distInit(){
+    for(int i=0; i<n; i++){
+        fill(dist[i],dist[i]+n,MAX);
     }
-    // for(int i=0; i<n; i++){
-    //     cout<<dist[i]<<" ";
-    // }
-    //test
-    //test
-    //shtest
-    cout<<"Problem "<<i<<": "<<dist[n-1]<<endl;
 }
+
+
 void Solve(){
     int i=1;
     while(1){
@@ -50,7 +29,7 @@ void Solve(){
         if(!n){
             break;
         }
-        Init();
+        distInit();
         
         for(int j=0; j<n; j++){
             for(int k=0; k<n; k++){
@@ -58,10 +37,36 @@ void Solve(){
             }
         }
         
-        Problem(i);
+        
+        priority_queue<piii,vector<piii>,greater<piii>>pq;
+        pq.push({arr[0][0],{0,0}});
+        dist[0][0]=arr[0][0];
+        while(!pq.empty()){
+            int hc=pq.top().first;
+            int hy=pq.top().second.first;
+            int hx=pq.top().second.second;
+            
+            pq.pop();
+
+            if(dist[hy][hx]<hc) continue;
+            
+            // cout<<i<<"hc : "<<hc<<" , hy : "<<hy<<" , hx : "<<hx<<endl;
+
+            for(int j=0; j<4; j++){
+                int ny=hy+dy[j];
+                int nx=hx+dx[j];
+                if(!isinside(ny,nx))continue;
+                int nc=hc+arr[ny][nx]; //현재위치까지의거리랑 다음까지거리
+                if(nc<dist[ny][nx]){
+                    dist[ny][nx]=nc;
+                    pq.push({nc,{ny,nx}});
+                }
+            }
+        }
+        cout<<"Problem "<<i<<": "<<dist[n-1][n-1]<<endl;
         i++;
     }
-}
+} //다익스트라사용
 int main(){
     ios::sync_with_stdio(false);
 	cin.tie(NULL);
