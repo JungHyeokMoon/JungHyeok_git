a187ef41e5b26b8cc7f480b9070f8ccee509c9db
MoonJungHyeok <wjdgur9534@daum.net>
2020-03-21 14:24:43 +0900

    4889 안정적인 문자열

---
+++ 1057.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+int n,k,i;
+int ret=0;
+void Input(){
+    cin>>n>>k>>i;
+}
+void Solve(){
+    while(1){
+        
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
