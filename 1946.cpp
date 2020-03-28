1877cba77d7d4616ce8495d3c36d2992faeb62ab
MoonJungHyeok <wjdgur9534@daum.net>
2020-03-28 12:10:58 +0900

    1946 주석추가

--- 1946.cpp
+++ 1946.cpp
@@ -14,13 +14,13 @@ void Solve(){
             cin>>v[i].first>>v[i].second;
         }
         
-        sort(v.begin(), v.end());
+        sort(v.begin(), v.end()); //정렬해서
         
         
         int cnt=1;
-        int temp=v[0].second;
+        int temp=v[0].second;//첫번째사람은 무조건 뽑힘 서류가1등이라서 면접점수 저장
         for(int i=1; i<n; i++){
-            if(temp>v[i].second){
+            if(temp>v[i].second){//첫번째서류사람보다 더 높은등수의 면접점수를 가지고있다면 초기화해주고 카운트증가
                 temp=v[i].second;
                 cnt++;
             }
