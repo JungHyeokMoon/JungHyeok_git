dc65d0ab0ae27f5097ef169b0eb63291d4c3b292
MoonJungHyeok <wjdgur9534@daum.net>
2020-07-01 23:56:49 +0900

    재귀 연습

--- 1074.cpp
+++ 1074.cpp
@@ -1,46 +1,78 @@
-#pragma warning(disable:4996)
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <queue>
-#include <stack>
-#include <math.h>
+// #pragma warning(disable:4996)
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <algorithm>
+// #include <queue>
+// #include <stack>
+// #include <math.h>
+// using namespace std;
+// int n, r, c;
+// int cnt = 0;
+// void Z(int x, int y,int size) {
+// 	if (x == r && y == c) {
+// 		cout << cnt << endl;
+// 		exit(0);
+// 	}
+// 	if (size == 1) {
+// 		cnt++;
+// 		return;
+// 	}
+// 	else {
+// 		if (!(x <= r && r < x + size && y <= c && c < y + size)) {
+// 			cnt += size * size;
+// 			return;
+// 		}
+// 		int newsize = size / 2;
+// 		Z(x, y, newsize);
+// 		Z(x, y + newsize, newsize);
+// 		Z(x + newsize, y, newsize);
+// 		Z(x + newsize, y + newsize, newsize);
+
+// 	}
+// }
+
+// int main() {
+// 	ios_base::sync_with_stdio(false);
+// 	cin.tie(NULL);
+// 	cout.tie(NULL);
+// 	cin >> n >> r >> c;
+// 	Z(0, 0, (int) pow(2,n));
+// 	return 0;
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
+#define endl "\n"
 int n, r, c;
-int cnt = 0;
-void Z(int x, int y,int size) {
-	if (x == r && y == c) {
-		cout << cnt << endl; 
-		exit(0);
+int k = 0;
+int Recursion(int n, int r, int c) //2^n * 2^n 배열에서 (r,c)를 방문하는 순서를 반환하는 함수
+{
+	if (n == 0)
+	{
+		return 0;
 	}
-	if (size == 1) {
-		cnt++;
-		return;
-	}
-	else {
-		if (!(x <= r && r < x + size && y <= c && c < y + size)) {
-			cnt += size * size;
-			return;
-		}
-		int newsize = size / 2;
-		Z(x, y, newsize);
-		Z(x, y + newsize, newsize);
-		Z(x + newsize, y, newsize);
-		Z(x + newsize, y + newsize, newsize);
-
-	}	
+	int half = (1 << (n - 1));
+	if (r < half && c < half)
+		return Recursion(n - 1, r, c);
+	if (r < half && c >= half)
+		return half * half + Recursion(n - 1, r, c - half);
+	if (r >= half && c < half)
+		return half * half * 2 + Recursion(n - 1, r - half, c);
+	else
+		return 3 * half * half + Recursion(n - 1, r - half, c - half);
 }
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+void Solve()
+{
 	cin >> n >> r >> c;
-	Z(0, 0, (int) pow(2,n));
+	cout << Recursion(n, r, c) << endl;
+}
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	Solve();
+	// cout << (1 << 1) << endl;
 	return 0;
 }
-
-
-
-
--- 11729.cpp
+++ 11729.cpp
@@ -1,41 +1,70 @@
-#pragma warning(disable:4996)
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <queue>
+// #pragma warning(disable:4996)
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <algorithm>
+// #include <queue>
+// using namespace std;
+
+// int n;
+
+// vector<pair<int, int>>v;
+
+// void hanoi(int start, int by, int end, int num) {
+// 	if (num == 1) {
+
+// 		//cout << start << " " << end << endl;
+// 		v.push_back(make_pair(start, end));
+// 	}
+// 	else {
+// 		hanoi(start, end, by, num - 1);
+
+// 		//cout << start << " " << end << endl;
+// 		v.push_back(make_pair(start, end));
+// 		hanoi(by, start, end, num - 1);
+// 	}
+// }
+// int main() {
+// 	ios_base::sync_with_stdio(false);
+// 	cin.tie(NULL);
+// 	cout.tie(NULL);
+// 	cin >> n;
+// 	hanoi(1,2,3,n);
+// 	int cnt = v.size();
+// 	cout << cnt << endl;
+// 	for (int i = 0; i < cnt; i++) {
+// 		cout << v[i].first << " " << v[i].second << '\n';
+// 	}
+
+// 	return 0;
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
-
+#define endl "\n"
 int n;
-
-vector<pair<int, int>>v;
-
-void hanoi(int start, int by, int end, int num) {
-	if (num == 1) {
-		
-		//cout << start << " " << end << endl;
-		v.push_back(make_pair(start, end));
+void hanoi(int a, int b, int n)
+{
+	if (n == 1)
+	{
+		cout << a << " " << b << endl;
+		return;
 	}
-	else {
-		hanoi(start, end, by, num - 1);
-		
-		//cout << start << " " << end << endl;
-		v.push_back(make_pair(start, end));
-		hanoi(by, start, end, num - 1);
-	}
+	hanoi(a, 6 - a - b, n - 1);
+	cout << a << " " << b << endl;
+	hanoi(6 - a - b, b, n - 1);
 }
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+void Solve()
+{
 	cin >> n;
-	hanoi(1,2,3,n);
-	int cnt = v.size();
-	cout << cnt << endl;
-	for (int i = 0; i < cnt; i++) {
-		cout << v[i].first << " " << v[i].second << '\n';
-	}
-	
-
+	cout << (1 << n) - 1 << endl;
+	hanoi(1, 3, n);
+}
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	Solve();
 	return 0;
 }
