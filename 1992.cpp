df292a0223f13f275a801a5c50e977cf5e53f088
MoonJungHyeok <wjdgur9534@daum.net>
2020-07-03 20:28:13 +0900

    1992 쿼드트리 재귀복습

--- 1992.cpp
+++ 1992.cpp
@@ -1,55 +1,114 @@
-#pragma warning(disable:4996)
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <queue>
-#include <stack>
+// #pragma warning(disable:4996)
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <algorithm>
+// #include <queue>
+// #include <stack>
+// using namespace std;
+// const int MAX = 65;
+// int n;
+// int arr[MAX][MAX];
+// int ans[3]; //0<=> -1 종이�??�� ,1<=> 0 종이�??�� , 2<=> 1 종이�??��
+// bool check(int x, int y, int len) {
+// 	int tmp = arr[x][y];
+// 	for (int i = x; i < x + len; i++) {
+// 		for (int j = y; j < y + len; j++) {
+// 			if (tmp != arr[i][j]) return false;
+// 		}
+// 	}
+// 	return true;
+// }
+// void quad(int x, int y, int len) {
+// 	if (check(x, y, len)) {
+// 		cout << arr[x][y];
+// 	}
+// 	else {
+// 		cout << "(";
+// 		int newlen = len / 2;
+
+// 		quad(x, y, newlen);
+// 		quad(x, y + newlen, newlen);
+// 		quad(x + newlen, y, newlen);
+// 		quad(x + newlen, y + newlen, newlen);
+// 		cout << ")";
+
+// 	}
+// }
+// int main() {
+// 	//ios_base::sync_with_stdio(false);
+// 	//cin.tie(NULL);
+// 	//cout.tie(NULL);
+// 	cin >> n;
+// 	for (int i = 0; i < n; i++) {
+// 		for (int j = 0; j < n; j++) {
+// 			scanf("%1d", &arr[i][j]);
+// 		}
+// 	}
+// 	quad(0, 0, n);
+// 	cout << endl;
+// 	return 0;
+// }
+
+#include <bits/stdc++.h>
+#define endl "\n"
 using namespace std;
-const int MAX = 65;
 int n;
-int arr[MAX][MAX];
-int ans[3]; //0<=> -1 종이갯수 ,1<=> 0 종이갯수 , 2<=> 1 종이갯수
-bool check(int x, int y, int len) {
-	int tmp = arr[x][y];
-	for (int i = x; i < x + len; i++) {
-		for (int j = y; j < y + len; j++) {
-			if (tmp != arr[i][j]) return false;
+vector<vector<char>> MAP;
+bool Check(int n, int y, int x)
+{
+	char first = MAP[y][x];
+	for (int i = y; i < y + n; i++)
+	{
+		for (int j = x; j < x + n; j++)
+		{
+			if (MAP[i][j] != first)
+			{
+				return false;
+			}
 		}
 	}
 	return true;
 }
-void quad(int x, int y, int len) {
-	if (check(x, y, len)) {
-		cout << arr[x][y];
-	}
-	else {
-		cout << "(";
-		int newlen = len / 2;
-
-		quad(x, y, newlen);
-		quad(x, y + newlen, newlen);
-		quad(x + newlen, y, newlen);
-		quad(x + newlen, y + newlen, newlen);
-		cout << ")";
-
-	}
-}
-int main() {
-	//ios_base::sync_with_stdio(false);
-	//cin.tie(NULL);
-	//cout.tie(NULL);
+void Init()
+{
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			scanf("%1d", &arr[i][j]);
+	MAP.resize(n, vector<char>(n));
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> MAP[i][j];
 		}
 	}
-	quad(0, 0, n);
-	cout << endl;
-	return 0;
+}
+void Recursion(int n, int y, int x)
+{
+	//base condition
+	if (Check(n, y, x))
+	{
+		cout << MAP[y][x];
+		return;
+	}
+	int divide = n / 2;
+	cout << "(";
+	Recursion(divide, y, x);
+	Recursion(divide, y, x + divide);
+	Recursion(divide, y + divide, x);
+	Recursion(divide, y + divide, x + divide);
+	cout << ")";
+}
+void Solve()
+{
+	Recursion(n, 0, 0);
 }
 
-
-
-
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	Init();
+	Solve();
+	return 0;
+}
