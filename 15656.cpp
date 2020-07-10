801f62894daf719e40186cdeccd46884af80b8cc
MoonJungHyeok <wjdgur9534@daum.net>
2020-07-11 03:04:17 +0900

    n과m복습

--- 15656.cpp
+++ 15656.cpp
@@ -1,31 +1,75 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int n,m;
-vector<int>ret;
-int arr[7];
-void n_m(int cnt){
-    if(cnt==m){
-        for(auto i : ret) cout<<i<<" ";
-        cout<<"\n";
-        return ;
+// int n,m;
+// vector<int>ret;
+// int arr[7];
+// void n_m(int cnt){
+//     if(cnt==m){
+//         for(auto i : ret) cout<<i<<" ";
+//         cout<<"\n";
+//         return ;
+//     }
+//     for(int i=0; i<n ; i++){
+//         ret.push_back(arr[i]);
+//         n_m(cnt+1);
+//         ret.pop_back();
+//     }
+// }
+// int main(){
+//     ios::sync_with_stdio(false);
+// 	cin.tie(NULL);
+
+//     cin>>n>>m;
+//     for(int i=0; i<n; i++)
+//         cin>>arr[i];
+
+//     sort(arr,arr+n);
+//     n_m(0);
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+vector<int> input;
+vector<int> ret;
+int n, m;
+void Init()
+{
+    cin >> n >> m;
+    input.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input[i];
     }
-    for(int i=0; i<n ; i++){
-        ret.push_back(arr[i]);
-        n_m(cnt+1);
-        ret.pop_back(); 
+    sort(input.begin(), input.end());
+}
+void Recursion()
+{
+    if (ret.size() == m)
+    {
+        for (auto i : ret)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        ret.push_back(input[i]);
+        Recursion();
+        ret.pop_back();
     }
 }
-int main(){
-    ios::sync_with_stdio(false);
-	cin.tie(NULL);
-    
-    cin>>n>>m;
-    for(int i=0; i<n; i++)
-        cin>>arr[i];
-
-    sort(arr,arr+n);
-    n_m(0);
-
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    Init();
+    Recursion();
     return 0;
 }
--- 15657.cpp
+++ 15657.cpp
@@ -1,29 +1,75 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int n,m;
+// vector<int>ret;
+// int arr[8];
+// void n_m(int start, int cnt){
+//     if(cnt==m){
+//         for(auto i :ret )cout<<i<<" ";
+//         cout<<"\n";
+//         return ;
+//     }
+//     for(int i=start; i<n ;i++){
+//         ret.push_back(arr[i]);
+//         n_m(i,cnt+1);
+//         ret.pop_back();
+//     }
+// }
+// int main(){
+//     ios::sync_with_stdio(false);
+// 	cin.tie(NULL);
+
+//     cin>>n>>m;
+//     for(int i=0; i<n ; i++){
+//         cin>>arr[i];
+//     }
+//     sort(arr,arr+n);
+//     n_m(0,0);
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
+#define endl "\n"
 using namespace std;
-int n,m;
-vector<int>ret;
-int arr[8];
-void n_m(int start, int cnt){
-    if(cnt==m){
-        for(auto i :ret )cout<<i<<" ";
-        cout<<"\n";
-        return ;
+vector<int> arr;
+vector<int> ret;
+int n, m;
+void Init()
+{
+    cin >> n >> m;
+    arr.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    for(int i=start; i<n ;i++){
+    sort(arr.begin(), arr.end());
+}
+void Recursion(int k)
+{
+
+    if (ret.size() == m)
+    {
+        for (auto i : ret)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    for (int i = k; i < n; i++)
+    {
         ret.push_back(arr[i]);
-        n_m(i,cnt+1);
+        Recursion(i);
         ret.pop_back();
     }
 }
-int main(){
-    ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-    cin>>n>>m;
-    for(int i=0; i<n ; i++){
-        cin>>arr[i];
-    }
-    sort(arr,arr+n);
-    n_m(0,0);
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    Init();
+    Recursion(0);
     return 0;
 }
