af84e283b993011eac73179c841c11fd1ac2f775
MoonJungHyeok <wjdgur9534@daum.net>
2020-07-11 17:16:12 +0900

    n과m복습 완료

--- 15664.cpp
+++ 15664.cpp
@@ -1,47 +1,113 @@
+// #include <bits/stdc++.h>
+// #define endl "\n"
+// using namespace std;
+// int n,m;
+// vector<bool> visited;
+// vector<int> arr;
+// vector<int> Combi;
+// set<string> s;
+// void Input(){
+//     cin>>n>>m;
+//     visited.resize(n,false);
+//     arr.resize(n);
+//     for(int i=0; i<n; i++){
+//         cin>>arr[i];
+//     }
+//     sort(arr.begin(),arr.end());
+// }
+// void Make_Combi(int depth,int idx){
+//     if(depth==m){
+//         string temp="";
+//         int length=Combi.size();
+//         for(int i=0; i<length; i++){
+//             temp+=(Combi[i]+'0');
+//             if(i!=length-1){
+//                 temp.push_back(' ');
+//             }
+//         }
+//         if(s.find(temp)==s.end()){
+//             s.insert(temp);
+//             for(int i=0; i<length; i++){
+//                 cout<<Combi[i]<<" ";
+//             }
+//             cout<<endl;
+//         }
+//         return ;
+//     }
+
+//     for(int i=idx; i<n; i++){
+//         if(!visited[i]){
+//             visited[i]=true;
+//             Combi.push_back(arr[i]);
+//             Make_Combi(depth+1,i+1);
+//             Combi.pop_back();
+//             visited[i]=false;
+//         }
+//     }
+// }
+// int main()
+// {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     Input();
+//     Make_Combi(0,0);
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 #define endl "\n"
 using namespace std;
-int n,m;
-vector<bool> visited;
+int n, m;
 vector<int> arr;
-vector<int> Combi;
+vector<bool> visited;
+vector<int> ret;
 set<string> s;
-void Input(){
-    cin>>n>>m;
-    visited.resize(n,false);
+void Init()
+{
+    cin >> n >> m;
     arr.resize(n);
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    visited.resize(n, false);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    sort(arr.begin(),arr.end());
+    sort(arr.begin(), arr.end());
+    visited.resize(10000, false);
 }
-void Make_Combi(int depth,int idx){
-    if(depth==m){
-        string temp="";
-        int length=Combi.size();
-        for(int i=0; i<length; i++){
-            temp+=(Combi[i]+'0');
-            if(i!=length-1){
-                temp.push_back(' ');
+void Recursion(int k)
+{
+    if (ret.size() == m)
+    {
+        string str = "";
+        for (int i = 0; i < ret.size(); i++)
+        {
+            str.push_back(ret[i] + '0');
+            if (i != ret.size() - 1)
+            {
+                str.push_back(' ');
             }
         }
-        if(s.find(temp)==s.end()){
-            s.insert(temp);
-            for(int i=0; i<length; i++){
-                cout<<Combi[i]<<" ";
+        if (s.find(str) == s.end())
+        {
+            s.insert(str);
+            for (int i = 0; i < ret.size(); i++)
+            {
+                cout << ret[i] << " ";
             }
-            cout<<endl;
+            cout << endl;
         }
-        return ;
+        return;
     }
-
-    for(int i=idx; i<n; i++){
-        if(!visited[i]){
-            visited[i]=true;
-            Combi.push_back(arr[i]);
-            Make_Combi(depth+1,i+1);
-            Combi.pop_back();
-            visited[i]=false;
+    for (int i = k; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            visited[i] = true;
+            ret.push_back(arr[i]);
+            Recursion(i + 1);
+            ret.pop_back();
+            visited[i] = false;
         }
     }
 }
@@ -50,7 +116,7 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    Input();
-    Make_Combi(0,0);
+    Init();
+    Recursion(0);
     return 0;
 }
--- 15665.cpp
+++ 15665.cpp
@@ -1,44 +1,101 @@
+// #include <bits/stdc++.h>
+// #define endl "\n"
+// using namespace std;
+// vector<int> arr;
+// vector<int> permu;
+// set<string> s;
+// int n,m;
+// void Input(){
+//     cin>>n>>m;
+//     arr.resize(n);
+//     for(int i=0; i<n; i++){
+//         cin>>arr[i];
+//     }
+//     sort(arr.begin(), arr.end());
+// }
+// void Make_Permu(int depth){
+//     if(depth==m){
+//         string temp="";
+//         int length =permu.size();
+//         for(int i=0; i<length; i++){
+//             temp+=(permu[i]+'0');
+//             if(i!=length-1){
+//                 temp.push_back(' ');
+//             }
+//         }
+
+//         if(s.find(temp)==s.end()){
+//             s.insert(temp);
+//             for(int i=0; i<length ; i++){
+//                 cout<<permu[i]<<" ";
+//             }
+//             cout<<endl;
+//         }
+//         return ;
+//     }
+
+//     for(int i=0; i<n; i++){
+//         permu.push_back(arr[i]);
+//         Make_Permu(depth+1);
+//         permu.pop_back();
+//     }
+// }
+// int main()
+// {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     Input();
+//     Make_Permu(0);
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 #define endl "\n"
 using namespace std;
+int n, m;
+vector<int> ret;
 vector<int> arr;
-vector<int> permu;
 set<string> s;
-int n,m;
-void Input(){
-    cin>>n>>m;
+void Init()
+{
+    cin >> n >> m;
     arr.resize(n);
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
     sort(arr.begin(), arr.end());
 }
-void Make_Permu(int depth){
-    if(depth==m){
-        string temp="";
-        int length =permu.size();
-        for(int i=0; i<length; i++){
-            temp+=(permu[i]+'0');
-            if(i!=length-1){
-                temp.push_back(' ');
+void Recursion()
+{
+    if (ret.size() == m)
+    {
+        string str = "";
+        for (int i = 0; i < ret.size(); i++)
+        {
+            str += (ret[i] + '0');
+            if (i != ret.size() - 1)
+            {
+                str.push_back(' ');
             }
         }
-
-        if(s.find(temp)==s.end()){
-            s.insert(temp);
-            for(int i=0; i<length ; i++){
-                cout<<permu[i]<<" ";
+        if (s.find(str) == s.end())
+        {
+            s.insert(str);
+            for (int i = 0; i < ret.size(); i++)
+            {
+                cout << ret[i] << " ";
             }
-            cout<<endl;
+            cout << endl;
         }
-        return ;
-    }
-    
-
-    for(int i=0; i<n; i++){
-        permu.push_back(arr[i]);
-        Make_Permu(depth+1);
-        permu.pop_back();
+        return;
+    } //base condition
+    for (int i = 0; i < n; i++)
+    {
+        ret.push_back(arr[i]);
+        Recursion();
+        ret.pop_back();
     }
 }
 int main()
@@ -46,7 +103,7 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    Input();
-    Make_Permu(0);
+    Init();
+    Recursion();
     return 0;
 }
