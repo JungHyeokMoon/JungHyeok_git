c52370677f2ae9e277edaa407442e566cc99011d
MoonJungHyeok <wjdgur9534@daum.net>
2020-08-07 01:45:05 +0900

    14888 연산자끼워넣기

--- 14888.cpp
+++ 14888.cpp
@@ -76,12 +76,54 @@
 #include <bits/stdc++.h>
 #define endl "\n"
 using namespace std;
+int n;
+vector<int> number;
+vector<int> Operator;
+int MAX=-1e9;
+int MIN=1e9;
+void Init(){
+	cin>>n;
+	number.resize(n);
+	for(int i=0; i<n; i++)
+		cin>>number[i];
+	
+	Operator.resize(4); //덧셈,뺄셈,곱셈,나눗셈
+	for(int i=0; i<4; i++)
+		cin>>Operator[i];
+}
+void dfs(int cnt, int sum){
+	if(cnt==n-1){
+		//MAX,MIN 계산하고
+		MAX=max(MAX,sum);
+		MIN=min(MIN,sum);
+		return ;
+	}
 
+	for(int i=0; i<4; i++){
+		if(Operator[i]!=0){
+			Operator[i]-=1;
+			if(i==0)
+				dfs(cnt+1,sum+number[cnt+1]);
+			else if(i==1)
+				dfs(cnt+1,sum-number[cnt+1]);
+			else if(i==2)
+				dfs(cnt+1,sum*number[cnt+1]);
+			else 
+				dfs(cnt+1,sum/number[cnt+1]);
+			Operator[i]+=1;
+		}
+	}
+}
+void Solve(){
+	dfs(0,number[0]);
+	cout<<MAX<<endl<<MIN<<endl;
+}
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-
+	Init();
+	Solve();
 	return 0;
 }
