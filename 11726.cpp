ec30249116d00bd598db0d4f4c89b1a77062a46d
MoonJungHyeok <wjdgur9534@daum.net>
2020-08-24 03:20:18 +0900

    11726 2*n 타일링

--- 11726.cpp
+++ 11726.cpp
@@ -1,21 +1,52 @@
-#include <iostream>
-#include <algorithm>
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+
+// int dp[1000 + 1];//2ï¿½ï¿½n Å©ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ç°¢ï¿½ï¿½ï¿½ï¿½ 1ï¿½ï¿½2, 2ï¿½ï¿½1 Å¸ï¿½Ï·ï¿½ Ã¤ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½Ì³ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ìºï¿½ï¿½ï¿½ï¿½Ö´ï¿½. dp[n]=dp[n-1]+dp[n-2];
+
+// int main(){
+// 	int n;
+// 	scanf("%d" ,&n);
+
+// 	dp[0] = 1; //2*0 ï¿½ï¿½Ã¤ï¿½ï¿½Â¹ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Î»ï¿½ï¿½ï¿½ï¿½Ï°ï¿½ ï¿½Ï³ï¿½ï¿½ï¿½ï¿½Ö´Ù°ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Ø¾ï¿½ï¿½ï¿½... 
+// 	dp[1] = 1;
+// 	for (int i = 2; i <= n; i++) {
+// 		dp[i] = dp[i - 1] + dp[i - 2];
+// 		dp[i] = dp[i] % 10007;
+// 	}
+
+// 	printf("%d\n", dp[n]);
+
+// 	return 0;
+// }
+
+#include <bits/stdc++.h>
+#define endl "\n"
+#define MOD 10007
 using namespace std;
+int n;
+vector<int> dp;
+void Init(){
+	cin>>n;
+	dp.resize(n+1,-1);
+	dp[1]=1 , dp[2]=2;
+}
+int Tiling(int n){
+	if(dp[n]!=-1)
+		return dp[n];
 
-int dp[1000 + 1];//2¡¿n Å©±âÀÇ Á÷»ç°¢ÇüÀ» 1¡¿2, 2¡¿1 Å¸ÀÏ·Î Ã¤¿ì´Â ¹æ¹ýÀÇ ¼ö °¡ ´ÙÀÌ³ª¹ÍÅ×ÀÌºí¿¡ÀÖ´Ù. dp[n]=dp[n-1]+dp[n-2];
-
-int main(){
-	int n;
-	scanf("%d" ,&n);
-
-	dp[0] = 1; //2*0 À»Ã¤¿ì´Â¹æ¹ýÀ» °øÁýÇÕÀ¸·Î»ý°¢ÇÏ°í ÇÏ³ª°¡ÀÖ´Ù°í »ý°¢ÇØ¾ßÇÔ... 
-	dp[1] = 1;
-	for (int i = 2; i <= n; i++) {
-		dp[i] = dp[i - 1] + dp[i - 2];
-		dp[i] = dp[i] % 10007;
-	}
-
-	printf("%d\n", dp[n]);
-
-	return 0;
+	dp[n]=(Tiling(n-1)%MOD+Tiling(n-2)%MOD)%MOD;
+	return dp[n];
+}
+void Solve(){
+	cout<<Tiling(n)<<endl;
+}
+int main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+   Init();
+   Solve();
+   return 0;
 }
