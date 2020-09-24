9ee8d1c6cf2056fd0a5bb0b25cc99c9039ba1324
MoonJungHyeok <wjdgur9534@daum.net>
2020-09-24 17:57:39 +0900

    17829 222-풀링

--- 17829.cpp
+++ 17829.cpp
@@ -1,65 +1,49 @@
-#pragma warning(disable:4996)
-#include <iostream>
-#include <algorithm>
-#include <cstring>
-#include <vector>
-#include <stack>
-#include <string>
-#include <cmath>
-#include <queue>
-#include <cstdlib>
-#include <map>
-#include <deque>
+#include <bits/stdc++.h>
+#define endl '\n'
 using namespace std;
 int n;
-int arr[1024][1024];
-vector<int> v;
-
-void solve(int y, int x, int length) {
-	
-	if(length==2) {
-		v[0] = arr[y][x];
-		v[1] = arr[y + 1][x];
-		v[2] = arr[y][x + 1];
-		v[3] = arr[y + 1][x + 1];
-		sort(v.begin(), v.end());
-		arr[y/2][x/2] = v[2];
-		
-	}
-	else  {
-		solve(y, x, length / 2);
-		solve(y, x + length / 2, length / 2);
-		solve(y + length / 2, x, length / 2);
-		solve(y + length / 2, x + length / 2, length / 2);
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			scanf("%d", &arr[i][j]);
+vector<vector<int>> MAP;
+int ret=0;
+int FindSecountNumber(int i, int j){
+	vector<int> temp;
+	for(int y=i; y<i+2; y++){
+		for(int x=j; x<j+2; x++){
+			temp.push_back(MAP[y][x]);
 		}
 	}
-	v.resize(4);
-	int cnt = 0;
-	int length = n;
-	while (n != 1) {
-		n /= 2;
-		cnt++;
+	sort(temp.begin(), temp.end());
+	return temp[2];
+}
+void Init(){
+	cin>>n;
+	MAP.resize(n,vector<int>(n));
+	for(auto & i :MAP)
+		for(auto & j : i)
+			cin>>j;
+}
+void Recursive(int n){
+	if(n==2){
+		ret=FindSecountNumber(0,0);
+		return ;
 	}
-	for (int i = 0; i < cnt; i++) {
-		solve(0, 0, length);
+
+	for(int i=0; i<n/2; i++){
+		for(int j=0; j<n/2; j++){
+			MAP[i][j]=FindSecountNumber(i*2,j*2);
+		}
 	}
-
-	printf("%d\n", arr[0][0]);
-
-
-	
-	
-
+	Recursive(n/2);
+}
+void Solve(){
+	Recursive(n);
+	cout<<ret<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+	Init();
+	Solve();
+	return 0;
 }
